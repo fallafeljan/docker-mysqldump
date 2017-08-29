d78f2579a6cdf4c85c96127f0ddc4a5e75411348
jk <fallafeljan@users.noreply.github.com>
2017-08-30 00:47:05 +0200

    add webhook fallback

--- job.sh
+++ job.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 DATE=`date +%Y-%m-%d`
+FILE="blog-${DATE}.sql"
 
-mysqldump -uroot -h $MYSQL_HOST $MYSQL_DATABASE \
-  | mc pipe "storage/${S3_BUCKET}/blog-${DATE}.sql"
+{
+  mysqldump -uroot -h $MYSQL_HOST $MYSQL_DATABASE \
+    | mc pipe "storage/${S3_BUCKET}/${FILE}" \
+    && echo "Created backup: ${FILE}"
+} || {
+  curl -X POST \
+    -H 'Content-type: application/json' \
+    --data '{"text": "Backup failed."}' \
+    $WEBHOOK_URL
+}
